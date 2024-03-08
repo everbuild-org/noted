a5682fb4423821da3db80f5176ba6deb532c17a5
Christian Bergschneider <christian.bergschneider@gmx.de>
2024-03-08 09:07:05 +0100

    feat: Load a rope

--- noted/src/ui/editor/mod.rs
+++ noted/src/ui/editor/mod.rs
@@ -3,18 +3,21 @@ use gpui::Render;
 use gpui::prelude::*;
 use gpui::View;
 use gpui::ViewContext;
+use ropey::Rope;
 use crate::markdown::parse_annotated_text_line;
 use crate::markdown::ui::line::MarkdownLineRenderer;
 
 use super::Shell;
 
 pub struct Editor {
-
+    content: Rope
 }
 
 impl Editor {
     pub fn build(cx: &mut ViewContext<Shell>) -> View<Self> {
-        cx.new_view(|_cx| Self {})
+        cx.new_view(|_cx| Self {
+            content: Rope::from_str(include_str!("../../../../data/welcome.md"))
+        })
     }
 
 }
@@ -24,19 +27,8 @@ impl Render for Editor {
         div()
             .flex_grow()
             .p_8()
-            .children(vec![
-                MarkdownLineRenderer::from(parse_annotated_text_line("# Heading 1")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("## Heading 2")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("### Heading 3")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("#### Heading 4")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("##### Heading 5")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("###### Heading 6")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("This is a paragraph of text")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("- This is an unordered list 1/2")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("- This is an unordered list 2/2")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("1. This is an ordered list 1/2")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("2. This is an ordered list 2/2")),
-                MarkdownLineRenderer::from(parse_annotated_text_line("This is another paragraph of text"))// long enough to wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like soo long that it should wrap around the screen and test the wrapping of the text, like so")),
-            ])
+            .children(self.content.lines().map(|line| {
+                MarkdownLineRenderer::from(parse_annotated_text_line(line.as_str().expect("Failed to parse line")))
+            }))
     }
 }
